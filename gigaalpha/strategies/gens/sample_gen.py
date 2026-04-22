b9a3211d6c1fbbe86e444c0b1f8387e8d66612ae
Thanhnt15 <thanhnt15@gmail.com>
2026-04-22 08:02:03 +0000

    fix logic gen sample

--- gigaalpha/strategies/gens/sample_gen.py
+++ gigaalpha/strategies/gens/sample_gen.py
@@ -6,10 +6,9 @@ from gigaalpha.core.registry import register_gen
     'threshold': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
     'half_life': [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
 })
-def gen_1_1(signal: pd.Series, threshold=0.3, half_life=0) -> pd.Series:
-    position = pd.Series(np.nan, index=signal.index)
-    position.loc[signal > threshold] = 1
-    position.loc[signal < -threshold] = -1
+def gen_sample(signal: pd.Series, threshold=0.3, half_life=0) -> pd.Series:
     if half_life > 0:
-        position = position.ewm(halflife=half_life).mean()
+        signal = signal.ewm(halflife=half_life).mean()
+    position = pd.Series(np.nan, index=signal.index)
+    position
     return position
